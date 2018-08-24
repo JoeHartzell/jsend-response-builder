b297567a629cc875e89cb2a59cedf75c6197b846
JoeHartzell <jhartzell@onhandschools.com>
2018-08-24 09:23:38 -0400

    Updated the builder to have a normalization process

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
+import { Case } from 'obj-key-normalizer';
 /**
  * a JSend status
  */
@@ -32,31 +33,51 @@ export interface IJSendErrorResponse<T> extends IJSendResponse {
     data?: T;
 }
 /**
+ * options for the JSendResponseBuilder
+ */
+export interface IJSendResponseBuilderOptions {
+    /**
+     * this sets which case type the response object keys will be
+     */
+    case?: Case;
+    /**
+     * this sets if we should deep normalize the response data or not
+     */
+    deep?: boolean;
+}
+/**
  * a class to build a JSend response
  */
-export declare class JSendResponseBuilder {
-    options: {};
+export default class JSendResponseBuilder {
+    private normalizer;
+    private _options;
+    options: IJSendResponseBuilderOptions;
     /**
      * creates an instance of a JSend builder
      * @constructor
      * @param options options for the builder
      */
-    constructor(options?: {});
+    constructor(options?: IJSendResponseBuilderOptions);
     /**
      * returns a well formed JSend success response
      * @param data data for success response
      */
-    success<T>(data: T): IJSendSuccessResponse<T>;
+    success<T>(data: T): IJSendSuccessResponse<any>;
     /**
      * returns a well formed JSend fail response
      * @param data data for fail response
      */
-    fail<T>(data: T): IJSendFailResponse<T>;
+    fail<T>(data: T): IJSendFailResponse<any>;
     /**
      * returns a well formed JSend error response
      * @param message error message
      * @param code error code (optional)
      * @param data data for error (optional)
      */
-    error<T = null>(message: string, code?: number, data?: T): IJSendErrorResponse<T>;
+    error<T = null>(message: string, code?: number, data?: T): IJSendErrorResponse<any>;
+    /**
+     * nomalizes a response
+     * @param response the response to normalize
+     */
+    private _normalizeResponse;
 }
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const obj_key_normalizer_1 = require("obj-key-normalizer");
 /**
  * a class to build a JSend response
  */
@@ -10,27 +11,57 @@ class JSendResponseBuilder {
      * @param options options for the builder
      */
     constructor(options = {}) {
+        this.normalizer = new obj_key_normalizer_1.Normalizer();
+        this._options = {};
         this.options = options;
+    }
+    set options(option) {
+        // set the normalizer options
+        this.normalizer.options.case = option.case;
+        this.normalizer.options.deep = option.deep;
+        this._options = option;
+    }
+    get options() {
+        return this._options;
     }
     /**
      * returns a well formed JSend success response
      * @param data data for success response
      */
     success(data) {
-        return {
+        // create response
+        const response = {
             data,
-            status: 'success'
+            status: 'success',
         };
+        // normalize the response
+        const normalized = this._normalizeResponse(data);
+        // if the normalized response data is not null
+        if (normalized) {
+            // set the response data to the normalized data
+            response.data = normalized;
+        }
+        // return the response
+        return response;
     }
     /**
      * returns a well formed JSend fail response
      * @param data data for fail response
      */
     fail(data) {
-        return {
+        // create response
+        const response = {
             data,
-            status: 'fail'
+            status: 'fail',
         };
+        // normalize response
+        const normalized = this._normalizeResponse(data);
+        // set the response data to the normalized data
+        if (normalized) {
+            response.data = normalized;
+        }
+        // return the response
+        return response;
     }
     /**
      * returns a well formed JSend error response
@@ -39,18 +70,33 @@ class JSendResponseBuilder {
      * @param data data for error (optional)
      */
     error(message, code, data) {
-        const result = {
+        // create the response
+        const response = {
             message,
-            status: 'error'
+            status: 'error',
         };
         if (code) {
-            result.code = code;
+            response.code = code;
         }
         if (data) {
-            result.data = data;
+            response.data = data;
         }
-        return result;
+        // normalize the response data
+        const normalized = this._normalizeResponse(data);
+        // set the normalized data if it exists
+        if (normalized) {
+            response.data = normalized;
+        }
+        // return the response
+        return response;
+    }
+    /**
+     * nomalizes a response
+     * @param response the response to normalize
+     */
+    _normalizeResponse(response) {
+        return this.normalizer.normalize(response).result;
     }
 }
-exports.JSendResponseBuilder = JSendResponseBuilder;
+exports.default = JSendResponseBuilder;
 //# sourceMappingURL=index.js.map
